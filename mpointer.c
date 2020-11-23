799ed2ba54fdabf41f3570bb1d1331b31acddc1c
Sunakim017 <sunattlina12@naver.com>
2020-11-23 17:53:19 +0900

    with comment

--- mpointer.c
+++ mpointer.c
@@ -7,48 +7,48 @@ void func4(int**** p);
 
 int main()
 {
-	int i = 100;
+	int i = 100;//declare variable
 
-	int *p, **dp, ***tp;
+	int *p, **dp, ***tp;//pointer variable
 
 	p = &i;
 	dp = &p;
 	tp = &dp;
 
 	func1(&i);
-	printf("i = %d\n", i);
+	printf("i = %d\n", i);//print func1 result
 
 	func2(&p);
-	printf("*p = %d\n",*p);
+	printf("*p = %d\n",*p);//print func2 result
 
 	func3(&dp);
-	printf("**dp = %d\n", **dp);
+	printf("**dp = %d\n", **dp);//print func3 result
 
 	func4(&tp);
-	printf("***tp = %d\n", ***tp);
+	printf("***tp = %d\n", ***tp);//print func4 result
 
-	printf("i = %d\n", i);
+	printf("i = %d\n", i);//print out final result
 	
-	return 0;
+	return 0;//function end
 
 }
 
 void func1(int* p)
 {
-	*p = *p+100;
+	*p = *p+100;//calculate 
 }
 
 void func2(int** p)
 {
-	**p = **p+100;
+	**p = **p+100;//calculate
 }
 
 void func3(int*** p)
 {
-	***p = ***p+100;
+	***p = ***p+100;//calculate
 }
 
 void func4(int**** p)
 {
-	****p = ****p+100;
+	****p = ****p+100;//calculate
 }
